e18872d115edd58d63de925296b18c507f470cf4
Jabed Kaium <jabedkaium@gmail.com>
2023-12-18 20:52:32 +0600

    first commit

---
+++ src/main/java/com/security/springsecurity/controllers/AuthenticationController.java
@@ -0,0 +1,27 @@
+package com.security.springsecurity.controllers;
+
+import com.security.springsecurity.models.ApplicationUser;
+import com.security.springsecurity.models.LoginResponseDTO;
+import com.security.springsecurity.models.RegistrationDTO;
+import com.security.springsecurity.services.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ApplicationUser registerUser(@RequestBody RegistrationDTO body) {
+        return authenticationService.registerUser(body.getUsername(), body.getPassword());
+    }
+
+    @PostMapping("/login")
+    public LoginResponseDTO loginUser(@RequestBody RegistrationDTO body) {
+        return authenticationService.loginUser(body.getUsername(), body.getPassword());
+    }
+}
